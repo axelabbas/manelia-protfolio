3d76f06ae4869f06543edec603f300a7dc6d7c81
axelabbas <Fadeloy45@gmail.com>
2023-07-21 13:58:46 +0300

    opacity, stop timer on hover

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,13 @@ const projectsSection = document.getElementById("projects");
 const sets = document.getElementById("sets");
 let currentIndex = 0;
 const cardsToShow = 4;
-const timerDuration = 10000; // 5 seconds
+const timerDuration = 10000; // 10 seconds
 let amountOfSets = projectCards.length / 4;
 let timerInterval;
 // Delay timings (in milliseconds)
 const fadeInDelay = 500;
 const displayChangeDelay = 500; // Adjust this value as needed
+let isTimerRunning = true;
 
 for (let i = 0; i < amountOfSets; i++) {
   let set = document.createElement("div");
@@ -18,7 +19,7 @@ for (let i = 0; i < amountOfSets; i++) {
     clearInterval(timerInterval);
     currentIndex = i * cardsToShow;
     showCards();
-    timerInterval = setInterval(nextSet, timerDuration);
+    timerInterval = setInterval(nextSetOrPause, timerDuration);
   };
   sets.appendChild(set);
 }
@@ -58,16 +59,42 @@ function showCards() {
   }, fadeInDelay);
 }
 
-function next() {
-  nextSet();
-  clearInterval(timerInterval);
-  timerInterval = setInterval(nextSet, timerDuration);
+function nextSetOrPause() {
+  if (isTimerRunning) {
+    currentIndex += cardsToShow;
+
+    if (currentIndex >= projectCards.length) {
+      currentIndex = 0;
+    }
+    showCards();
+  }
 }
+
+function handleCardHover() {
+  isTimerRunning = false;
+}
+
+function handleCardHoverEnd() {
+  isTimerRunning = true;
+}
+
+for (let i = 0; i < projectCards.length; i++) {
+  projectCards[i].addEventListener("mouseover", handleCardHover);
+  projectCards[i].addEventListener("mouseout", handleCardHoverEnd);
+}
+
+function next() {
+  nextSetOrPause();
+  clearInterval(timerInterval);
+  timerInterval = setInterval(nextSetOrPause, timerDuration);
+}
+
 function prev() {
   prevSet();
   clearInterval(timerInterval);
-  timerInterval = setInterval(nextSet, timerDuration);
+  timerInterval = setInterval(nextSetOrPause, timerDuration);
 }
+
 function nextSet() {
   currentIndex += cardsToShow;
 
@@ -87,7 +114,6 @@ function prevSet() {
 }
 
 // Start the timer to automatically show the next set of cards
-timerInterval = setInterval(nextSet, timerDuration);
-
+timerInterval = setInterval(nextSetOrPause, timerDuration);
 
 showCards();
